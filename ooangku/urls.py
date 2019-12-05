5f95a51a3cd6dc8f317b403570fa34b75fd6f440
raihan2604 <raihan2604@gmail.com>
2019-12-05 16:53:33 +0700

    initial commit

--- ooangku/urls.py
+++ ooangku/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('financialPlanner.urls')),
+    path('budgeting/', include('budgeting.urls')),
+    path('income/', include('income.urls')),
+    path('liability/', include('liability.urls')),
+    path('taxCounter/', include('taxCounter.urls')),
+    path('account/', include('django.contrib.auth.urls'))
 ]
